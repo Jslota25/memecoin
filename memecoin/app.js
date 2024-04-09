ebff784f56cd5e2e946a53a9806f1e73bbecc0e8
John Slota <johnslota@Johns-MacBook-Pro.local>
2024-04-08 21:47:04 -0500

    made navbar pink

---
+++ memecoin/app.js
@@ -0,0 +1,10 @@
+var buybutton = document.getElementById("Buy");
+buyButton.addEventListener("click", pushButton);
+var chartbutton = document.getElementById("Chart")
+var followbutton = document.getElementById("Follow")
+
+
+
+function pushButton() {
+    buybutton
+}
